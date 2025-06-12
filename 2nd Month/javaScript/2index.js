efb8183d5fdd0f98db1810d9e59445b662de9eb5
pournima0212 <pournimamane0217@gmail.com>
2025-06-12 19:58:36 +0530

    function array object string concept done

---
+++ 2nd Month/javaScript/2index.js
@@ -0,0 +1,25 @@
+//Variables
+
+let Name = "Pournima";
+const SecondName = "Vilas";
+var surName = "Mane";
+
+//Functions
+
+function addNumbers(a,b) {
+    console.log(a+b);
+}
+
+//Arrys
+
+const firstArray= [1,3,5,7]
+
+//Objects
+
+const firstObjecct = {
+    name: "Pournima"
+ }
+
+ //String
+
+ const firstString = "Pournima Mane";
