a1471d059d1431090b7cdfdc75f88a66f9483a51
pournima0212 <pournimamane0217@gmail.com>
2025-06-12 20:08:18 +0530

    done

--- 2nd Month/javaScript/1index.js
+++ 2nd Month/javaScript/1index.js
@@ -1,4 +1,8 @@
 // console.log("My Name is Pournima")
+
+// const firstName= "Pournima";
+// const SecondName= "Mane";
+// const lastName= "Mane";
 function writefullName(Name, fatherName,lastName) {
 
     const fullName = Name + fatherName + lastName;
@@ -6,4 +10,5 @@ function writefullName(Name, fatherName,lastName) {
     console.log(fullName);
 }
 
+// console.log(writefullName( "Pournima " , "Vilas " , "Mane" ));
 writefullName( "Pournima " , "Vilas " , "Mane" );
