c72230487ea6e17a13119746aa5d6663ea247d39
pournima0212 <pournimamane0217@gmail.com>
2025-06-19 20:19:21 +0530

    done with usestate

---
+++ 2nd Month/ReactJs/firstdemo/src/App.jsx
@@ -0,0 +1,31 @@
+
+import { useState } from "react";
+
+function App() {
+ const[ name , setName ] = useState()
+ const[ showName , setShowName] = useState(false)
+
+ console.log(name)
+
+ const handleOnChange = (e) =>{
+console.log(e.target.value, "===")
+setName(e.target.value)
+ }
+
+ const handleSubmit = () =>{
+  setShowName(true)
+ }
+  return (
+    <>
+
+    <input name= "name" placeholder="Enter Your Name" onChange={handleOnChange}/>
+
+    <button onClick={handleSubmit} >Submit</button>
+
+    {showName && <h1>{name}</h1>}
+
+    </>
+  )
+}
+
+export default App
