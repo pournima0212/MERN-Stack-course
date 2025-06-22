69d1297ec1210f123af92229204b793840cafe3e
pournima0212 <pournimamane0217@gmail.com>
2025-06-23 00:05:27 +0530

    done with props concept

--- 2nd Month/ReactJs/firstdemo/src/App.jsx
+++ 2nd Month/ReactJs/firstdemo/src/App.jsx
@@ -1,29 +1,50 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from "react";
+import Card from './Card';
 
 function App() {
-  const [name, setName] = useState()
+  const [name, setName] = useState(); //Hooks
+
   const [showName, setShowName] = useState(false);
 
   const handleOnChange = (e) => {
-    console.log(e.target.value, "===")
+    console.log(e.target.value);
     setName(e.target.value);
   };
 
   const handleSubmit = () => {
     setShowName(true);
   };
+
+  const [cardData, setCardData] = useState(
+    [
+      { title: "Title - 1" },
+      { title: "Title - 2" },
+      { title: "Title - 3" },
+      { title: "Title - 4" },
+    ]
+  );
+
+  
   return (
     <>
+    
+      <input
+        name="name"
+        placeholder="Enter Your Name"
+        onChange={handleOnChange}
+      />
 
-      <input name="name" placeholder="Enter Your Name" onChange={handleOnChange} />
-
-      <button onClick={handleSubmit} >Submit</button>
-
+      <button onClick={handleSubmit}>Submit</button>
       {showName && <h1>{name}</h1>}
+
+      {cardData.map((each) => (
+        <Card title={each.title} />
+      ))}
 
     </>
   );
 }
 
 export default App
+
