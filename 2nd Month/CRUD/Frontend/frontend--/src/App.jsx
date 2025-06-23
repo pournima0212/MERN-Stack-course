42c3995b59d829ae1fdf38f9885c5a4f059a0151
pournima0212 <pournimamane0217@gmail.com>
2025-06-23 17:26:18 +0530

    done with form and table for CRUD

---
+++ 2nd Month/CRUD/Frontend/frontend--/src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css'
+import Student from "./pages/Student"
+
+function App() {
+
+  return (
+    <>
+    <Student/>
+    </>
+  )
+}
+
+export default App
---
+++ 2nd Month/CRUD/Frontend/frontend--/src/pages/Student.jsx
@@ -0,0 +1,111 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Table from 'react-bootstrap/Table';
+
+const Student = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="add-form-div">
+                            <h2 className="text-center">Add Student</h2>
+                            <Form>
+                                <Row className="mb-3">
+                                    <Form.Group as={Col}>
+                                        <Form.Label>First Name</Form.Label>
+                                        <Form.Control type="text" placeholder="Enter First Name" />
+                                    </Form.Group>
+
+                                    <Form.Group as={Col}>
+                                        <Form.Label>Last Name</Form.Label>
+                                        <Form.Control type="text" placeholder="Enter Last Name" />
+                                    </Form.Group>
+                                </Row>
+
+                                <Row className="mb-3">
+                                    <Form.Group as={Col}>
+                                        <Form.Label>Std</Form.Label>
+                                        <Form.Control type="text" placeholder="Enter Standard" />
+                                    </Form.Group>
+
+                                    <Form.Group as={Col}>
+                                        <Form.Label>Roll No.</Form.Label>
+                                        <Form.Control type="text" placeholder="Enter Roll No." />
+                                    </Form.Group>
+                                </Row>
+
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Address</Form.Label>
+                                    <Form.Control placeholder="1234 Main Street" />
+                                </Form.Group>
+
+                                <Row className="mb-3">
+                                    <Form.Group as={Col}>
+                                        <Form.Label>City</Form.Label>
+                                        <Form.Control />
+                                    </Form.Group>
+
+                                    <Form.Group as={Col}>
+                                        <Form.Label>State</Form.Label>
+                                        <Form.Select defaultValue="Choose...">
+                                            <option>Choose...</option>
+                                            <option>...</option>
+                                        </Form.Select>
+                                    </Form.Group>
+
+                                    <Form.Group as={Col}>
+                                        <Form.Label>Zip</Form.Label>
+                                        <Form.Control />
+                                    </Form.Group>
+                                </Row>
+
+                                <Button variant="primary" type="submit">
+                                    Submit
+                                </Button>
+                            </Form>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="add-form-div">
+                            <h2 className="text-center">Students List</h2>
+                            <Table striped bordered hover>
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>First Name</th>
+                                        <th>Last Name</th>
+                                        <th>Std</th>
+                                        <th>Roll No.</th>
+                                        <th>Address</th>
+                                        <th>City</th>
+                                        <th>State</th>
+                                        <th>Zip</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Pournima</td>
+                                        <td>Mane</td>
+                                        <td>10th</td>
+                                        <td>09</td>
+                                        <td>Karad</td>
+                                        <td>Karad</td>
+                                        <td>Maharashtra</td>
+                                        <td>415110</td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Student
