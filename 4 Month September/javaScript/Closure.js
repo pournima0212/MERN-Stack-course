912e369bfa26ca32e52aff640a26a63d86268068
pournima0212 <pournimamane0217@gmail.com>
2025-09-02 18:58:08 +0530

    done with closer

---
+++ 4 Month September/javaScript/Closure.js
@@ -0,0 +1,28 @@
+console.log("helo")
+
+function firstFunction() {
+
+    const name = "pournima"
+    
+
+    function secondFunction() {
+
+        const surName = "Mane"
+        var number = 10;
+        if(true){
+            var finalNumber= 100;
+            const a = "10";
+        }
+console.log(finamNumber)
+console.log(number)
+        console.log(name + " " + surName)
+
+    };
+    console.log(name, "name");
+    console.log(surName, "surname");
+    secondFunction();
+};
+
+firstFunction();
+
+// closer is nothing but nested function caliing function inside function 
