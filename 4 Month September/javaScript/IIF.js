21b205f0508a12b2c630cf6c06284880a7067336
pournima0212 <pournimamane0217@gmail.com>
2025-09-11 19:45:07 +0530

    invok function done

---
+++ 4 Month September/javaScript/IIF.js
@@ -0,0 +1,13 @@
+//imidiatly invok function
+ (
+    function(){
+        console.log("imidiately invok function")
+    }
+ )()
+
+
+ //self invok function
+
+ (function(){
+    console.log("self invok function")
+ }())
